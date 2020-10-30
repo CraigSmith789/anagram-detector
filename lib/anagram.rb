f6373ce79b6bb4cd8ee4ae5e91bc1de0148179fb
CraigSmith789 <craig789@gmail.com>
2020-10-30 14:58:46 -0400

    commit tree 927cad62ceda41bae968ae86bd91fc8cf82339ff

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word
+def initialize(word)
+    @word = word
+end
+
+def match(array)
+    new_array = []
+    array.map do |item|
+    
+        if @word.split(//).sort === item.split(//).sort
+            new_array<< item 
+                        
+        end
+    end
+        new_array
+       
+      
+    end
+end
